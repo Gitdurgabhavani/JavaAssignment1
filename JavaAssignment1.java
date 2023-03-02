6b2f7a764216d63d5a5f8a730ef4db5b2437c2a3
Gitdurgabhavni <durgabhavani33312@gmail.com>
2023-03-02 21:08:10 +0530

    JavaAssignment

---
+++ JavaAssignment1.java
@@ -0,0 +1,21 @@
+1.What is a programming language?
+Ans. The set of instructions that instruct a computer how to carry out a task.
+
+2.Why do we need a programming language?
+Ans.Programming language is important in our daily life to enhance and increase the power of computers mobile solutions 
+and the internet. 
+
+3.What are the features of java?
+Ans. Object-oriented , platform independent , simple, portable , Robust.
+
+4.What is an object?
+Ans.An object is a real world entity which has a state and behaviour.
+
+5.what is a class?
+Ans.A Class is a collection of items with similar characters.
+
+6.Explain about the main() method in java?
+Ans.The main() is the starting point for JVM to start execution of java prrogram. 
+---Without the main() method the jvm will not execute the program
+---The syntax for main method is
+     public static void main(String args[])
